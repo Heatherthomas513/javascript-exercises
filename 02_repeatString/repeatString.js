4b302bccec00b7f319e43c9b71eb91b51b6ae508
heatherthomas513 <heatherthomas513@gmail.com>
2023-11-28 15:04:39 -0500

    Complete function
    Completed repeatString function using if/else conditions, a for loop, and .push and .join methods to create the result variable array and convert it to a string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let result = [];
+        for (let i = 0; i < num; i++) {
+            result.push(string);
+        }
+        return result.join("");
+    } 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
