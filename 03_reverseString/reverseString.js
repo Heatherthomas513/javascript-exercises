ca5084c650cc43201f38044e0affe579f8e90476
heatherthomas513 <heatherthomas513@gmail.com>
2023-11-28 16:10:08 -0500

    Complete function to reverse strings
    Using a for loop the function pushes the characters of the string into an array one by one starting at the end, and then joins them back together into a string using .join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = [];
+    for (let i = 1; i <= string.length; i++) {
+        result.push(string.substr(string.length-i, 1));
+    }
+    return result.join("");
 };
 
 // Do not edit below this line
