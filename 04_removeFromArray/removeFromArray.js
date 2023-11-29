af63ace6ef4c069b7389491ab9093eef43fb5696
heatherthomas513 <heatherthomas513@gmail.com>
2023-11-29 09:19:14 -0500

    Complete function
    Used the rest parameter so that the function could accept an indefinite number of arguments. The for loop runs a filter for each argument to be removed, filtering them out of the array and then returning the new array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+    for (i = 0; i < removeArgs.length; i++) {
+        array = array.filter(value => {
+            return value !== removeArgs[i];
+        }); 
+    }; return array;
 };
 
 // Do not edit below this line
