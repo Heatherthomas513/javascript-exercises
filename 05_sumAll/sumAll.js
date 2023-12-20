db969884d75e2c8d4168e2027d37960aa66ccbd1
heatherthomas513 <heatherthomas513@gmail.com>
2023-12-20 16:05:29 -0500

    Add function for summing numbers in range
    Use if/else condition to run the function only if a and b are non-negative integers, otherwise it returns 'ERROR'. a and b are first added to a variable titled array, then sorted so that the function will work even if a is larger. The function runs a for loop to add all numbers in the range to a fullArray variable, and then sums them using the forEach method.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0) {
+        let array = [a, b];
+        array.sort();
+        let fullArray = [];
+        for (i = array[0]; i <= array[1]; i++) {
+            fullArray.push(i);
+       } let sum = 0;
+       fullArray.forEach((el) => sum += el);
+       return sum;
+    } else {
+        return 'ERROR';
+    };
 };
 
 // Do not edit below this line
