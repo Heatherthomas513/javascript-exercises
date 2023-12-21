640bd3d26a145be87eaac86fc3818413adba7ad6
heatherthomas513 <heatherthomas513@gmail.com>
2023-12-21 07:31:27 -0500

    Write conversion functions
    Write functions for converting temperatures and returning the number rounded to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  tempF = (tempF - 32) * 5/9;
+  return tempF = Math.round(tempF * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  tempC = (tempC * 9/5 + 32)
+  return tempC = Math.round(tempC * 10)/10;
 };
 
 // Do not edit below this line
